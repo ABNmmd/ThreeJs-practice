7f8109c8bbbfda4fdb1652ef64942ddbf027117b
ABNmmd <moha.ww.ha@gmail.com>
2024-09-17 22:40:30 +0100

    THREE js basics

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
 
 const scene = new THREE.scene();
+
+const geometry = THREE.BoxGeometry(1, 1, 1);
+
