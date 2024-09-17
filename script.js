db3316f0198cfbb84e250848c3d7e9ce30fbd82a
ABNmmd <moha.ww.ha@gmail.com>
2024-09-17 22:43:54 +0100

    THREE js basics

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const canvas = document.getElementById(canvas);
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
 const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
-
 const scene = new THREE.scene();
 
 const geometry = THREE.BoxGeometry(1, 1, 1);
-
 const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
 
+cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+renderer.render(scene, camera);
