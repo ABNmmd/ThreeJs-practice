657b93ae665f8bd529e2ff5dd6db7c2338331c6b
ABNmmd <moha.ww.ha@gmail.com>
2024-09-17 22:58:16 +0100

    THREE js basics

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
 const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+camera.position.z = 2;
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -13,5 +14,14 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 
+function render(time) {
+    time *= 0.001;
 
-renderer.render(scene, camera);
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
