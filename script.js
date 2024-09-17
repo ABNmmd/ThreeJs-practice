4d8fdeef99adc7afeabf6220c6f715a21859facf
ABNmmd <moha.ww.ha@gmail.com>
2024-09-17 22:39:50 +0100

    THREE js basics

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ import * as THREE from 'three';
 
 const canvas = document.getElementById(canvas);
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+
+const scene = new THREE.scene();
