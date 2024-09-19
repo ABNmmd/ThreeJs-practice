027458b1a5deff451b3091d73cdc890d50111d34
ABNmmd <moha.ww.ha@gmail.com>
2024-09-19 23:00:26 +0100

    THREE js basics

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+
+//rendrer
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
+//camera
 const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
 camera.position.z = 4;
 const scene = new THREE.Scene();
@@ -11,14 +14,20 @@ const orbitControl = new OrbitControls(camera, canvas);
 
 const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
-
 const earth = new THREE.Mesh(geometry, material);
 
+
+
+//lightning
 const light = new THREE.DirectionalLight(0xFFFFFF, 3);
 light.position.set(-1, 2, 4);
 
+
+// adding to the scene
 scene.add(earth);
 scene.add(light);
+
+
 
 function render(time) {
     time *= 0.001;
